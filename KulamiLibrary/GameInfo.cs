1074e96bc1b6749beb356522e95cd73628d125b4
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2023-09-16 18:11:10 +0200

    Write GameInfo struct

---
+++ KulamiLibrary/GameInfo.cs
@@ -0,0 +1,13 @@
+﻿using KulamiClassLibrary;
+
+namespace KulamiLibrary
+{
+    public struct GameInfo
+    {
+        public Board Board { get; private set; }
+        public PlayerNumber CurrentPlayer { get; private set; }
+        public List<Socket> PossibleMoves { get; private set; }
+        public int Turn { get; private set; }
+
+    }
+}
